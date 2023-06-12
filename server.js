db1efef6d95797a8f26c924e188780f5897a7792
AdriAguilar <adritrivi41@gmail.com>
2023-06-12 22:56:59 +0200

    chat works!

--- server.js
+++ server.js
@@ -1,36 +1,40 @@
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
-const redis = require('redis');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {cors: {origin: '*'}});
-const redisClient = redis.createClient();
 
 io.on('connection', (socket) => {
   console.log('-- A user connected --');
 
   // Manejar el evento "join" cuando un usuario se une a un chat
   socket.on('join', (chatId, username) => {
-    console.log(`User ${username} joined chat ${chatId}`);
+    console.log(`${username} joined chat ${chatId}`);
     socket.join(chatId);
   });
 
   // Manejar el evento "chat message" cuando un usuario envía un mensaje
-  socket.on('sendMessage', (chatId, message) => {
-    console.log(`A user sent message: ${message}`);
+  socket.on('sendMessage', (chatId, message, username, date, userId) => {
+    console.log(`${username} sent message on chat ${chatId}: ${message}`);
     const chatMessage = {
       chat: chatId,
-      message: message
+      message: message,
+      username: username,
+      date: date,
+      isMyMessage: true,
+      user: {
+        id: userId
+      }
     };
-    // redisClient.rPush(`chat:${chatId}`, JSON.stringify(chatMessage));
+    // Devuelve el mensaje a los demás usuarios del chat
     io.to(chatId).emit('receiveMessage', chatMessage);
   });
 
   // Manejar el evento "leave" cuando un usuario sale de un chat
   socket.on('leave', (chatId, username) => {
-    console.log(`User ${username} left chat ${chatId}`);
+    console.log(`${username} left chat ${chatId}`);
     socket.leave(chatId);
   });
 });
